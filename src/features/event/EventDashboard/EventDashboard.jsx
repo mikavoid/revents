90c11587e55e17dc2214cf4ea822e4e28d8ba062
Mickael BOULAT <mikavoid@gmail.com>
2019-03-14 14:50:32 +0100

    Add prettier and EventDashboard

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { Component } from 'react'
 import { Grid } from 'semantic-ui-react'
 
@@ -5,10 +7,10 @@ class EventDashboard extends Component {
   render() {
     return (
       <Grid>
-        <Grid.Column with={10}>
+        <Grid.Column width={10}>
           <h2>Left Column</h2>
         </Grid.Column>
-        <Grid.Column with={6}>
+        <Grid.Column width={6}>
           <h2>Right Column</h2>
         </Grid.Column>
       </Grid>
@@ -17,4 +19,3 @@ class EventDashboard extends Component {
 }
 
 export default EventDashboard
-
